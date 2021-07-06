947aa9d6412475aac9e0b1e36644066bc10a0f96
wiladhianty <wiladhianty@gmail.com>
2021-07-06 12:30:51 +0700

    tambah tugas

---
+++ tugas1.php
@@ -0,0 +1,11 @@
+<?php
+    $hasil = (36*12)/24;
+    echo "
+        <p>
+            36 Hari = 12 Pekerja<br/>
+            X hari = 24 Pekerja<br/>
+            X = ...?<br/>
+            Jawaban : $hasil
+        </p>
+    ";
+?>
---
+++ tugas2.php
@@ -0,0 +1,13 @@
+<?php
+    $jual = 80000;
+    $beli = 50000;
+    $hasil = $jual- $beli;
+    echo "
+        <p>
+            Jual = $jual <br/>
+            Beli = $beli <br/>
+            Keuntungan = ? <br/>
+            Jawaban : $hasil
+        </p>
+    ";
+?>
